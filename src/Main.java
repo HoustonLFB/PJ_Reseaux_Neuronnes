404b50ad75a1bea85e7a4425f1535d577834c6bf
HoustonLFB <23219631+HoustonLFB@users.noreply.github.com>
2024-05-24 11:21:08 +0200

    clean

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,22 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        // Création des neurones
+        //ET logique
+        //etLogique();
+
+        //OU logique
+        //
+        // ouLogique();
+
+        //NO logique
+        //noLogique();
+        }
+
+public static void ouLogique() {
         NeuroneEntree neuroneEntree1 = new NeuroneEntree(new double[]{1.0}, 0.0);
         NeuroneEntree neuroneEntree2 = new NeuroneEntree(new double[]{1.0}, 0.0);
         //Neurone.NeuroneCache neuroneCache = new Neurone.NeuroneCache(new double[]{1.0, 1.0}, 0.0);
-        NeuroneSortie neuroneSortie = new NeuroneSortie(new double[]{1.0, 1.0}, -1.0);
+        NeuroneSortie neuroneSortie = new NeuroneSortie(new double[]{1.0, 1.0}, -2.0);
 
         // Création des couches
         CoucheEntree coucheEntree = new CoucheEntree(new NeuroneEntree[]{neuroneEntree1, neuroneEntree2});
@@ -26,7 +37,52 @@ public class Main {
         Reseau reseau = new Reseau(new CoucheEntree[]{coucheEntree}, null, new CoucheSortie[]{coucheSortie});
 
         // Test du réseau avec des échantillons
-        double[][] echantillons = {{0,0}, {0,1}, {1,0}, {1,1}};
+        double[][] echantillons = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        for (double[] echantillon : echantillons) {
+            double[] sortie = reseau.feedForward(echantillon);
+            System.out.println("Entrée : " + Arrays.toString(echantillon) + ", Sortie : " + Arrays.toString(sortie));
+        }
+    }
+        public static void etLogique() {
+            NeuroneEntree neuroneEntree1 = new NeuroneEntree(new double[]{1.0}, 0.0);
+            NeuroneEntree neuroneEntree2 = new NeuroneEntree(new double[]{1.0}, 0.0);
+            //Neurone.NeuroneCache neuroneCache = new Neurone.NeuroneCache(new double[]{1.0, 1.0}, 0.0);
+            NeuroneSortie neuroneSortie = new NeuroneSortie(new double[]{1.0, 1.0}, -2.0);
+
+            // Création des couches
+            CoucheEntree coucheEntree = new CoucheEntree(new NeuroneEntree[]{neuroneEntree1, neuroneEntree2});
+            //Couche.CoucheCachee coucheCachee = new Couche.CoucheCachee(new Neurone.NeuroneCache[]{neuroneCache});
+            CoucheSortie coucheSortie = new CoucheSortie(new NeuroneSortie[]{neuroneSortie});
+
+            // Création du réseau
+            //Reseau reseau = new Reseau(new Couche.CoucheEntree[]{coucheEntree}, new Couche.CoucheCachee[]{coucheCachee}, new Couche.CoucheSortie[]{coucheSortie});
+            Reseau reseau = new Reseau(new CoucheEntree[]{coucheEntree}, null, new CoucheSortie[]{coucheSortie});
+
+            // Test du réseau avec des échantillons
+            double[][] echantillons = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+            for (double[] echantillon : echantillons) {
+                double[] sortie = reseau.feedForward(echantillon);
+                System.out.println("Entrée : " + Arrays.toString(echantillon) + ", Sortie : " + Arrays.toString(sortie));
+            }
+        }
+
+    public static void noLogique() {
+        NeuroneEntree neuroneEntree1 = new NeuroneEntree(new double[]{1.0}, 0.0);
+        NeuroneEntree neuroneEntree2 = new NeuroneEntree(new double[]{1.0}, 0.0);
+        //Neurone.NeuroneCache neuroneCache = new Neurone.NeuroneCache(new double[]{1.0, 1.0}, 0.0);
+        NeuroneSortie neuroneSortie = new NeuroneSortie(new double[]{1.0, -2.0}, 0.0);
+
+        // Création des couches
+        CoucheEntree coucheEntree = new CoucheEntree(new NeuroneEntree[]{neuroneEntree1, neuroneEntree2});
+        //Couche.CoucheCachee coucheCachee = new Couche.CoucheCachee(new Neurone.NeuroneCache[]{neuroneCache});
+        CoucheSortie coucheSortie = new CoucheSortie(new NeuroneSortie[]{neuroneSortie});
+
+        // Création du réseau
+        //Reseau reseau = new Reseau(new Couche.CoucheEntree[]{coucheEntree}, new Couche.CoucheCachee[]{coucheCachee}, new Couche.CoucheSortie[]{coucheSortie});
+        Reseau reseau = new Reseau(new CoucheEntree[]{coucheEntree}, null, new CoucheSortie[]{coucheSortie});
+
+        // Test du réseau avec des échantillons
+        double[][] echantillons = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
         for (double[] echantillon : echantillons) {
             double[] sortie = reseau.feedForward(echantillon);
             System.out.println("Entrée : " + Arrays.toString(echantillon) + ", Sortie : " + Arrays.toString(sortie));
